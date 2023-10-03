007d656fd91d8610823b64c5e8b830c668d688ab
1uhCarl <111576320+1uhCarl@users.noreply.github.com>
2023-10-03 13:46:36 -0400

    mr kelly help

---
+++ computer_science_exercises/00_helloWorld/helloWorld.cs
@@ -0,0 +1,26 @@
++// Carlton Brinley, Hello World, v0.1
+using System;
+class HelloWorld {
+  static void Main() {
+    Console.WriteLine("Hello World");
+  }
+}
+
+
+
+
+       // Data types
+       // Integers -- +/-Whole numbers
+       // Float -- +/- decimal numbers
+       // String -- collection of letters, numbers, characters inside " "
+       // Boolean -- True or false vaues. 
+
+       // DATA TYPES MUST BE DECLARED AND CANNOT CHANGE !
+       int MYaGE = 18;
+       int myHeight;
+       int myName Carlton
+       
+       
+
+
+   
