344aa2041f9e328ba2d8a7fe22006e6d419cbbfb
1uhCarl <111576320+1uhCarl@users.noreply.github.com>
2023-10-24 14:32:14 -0400

    temole.02

---
+++ 04a/programTemplate.cs
@@ -0,0 +1,44 @@
+// Carlton Brinkley, Program Template, v0.01
+using system;
+
+namespace MethodParameters
+{
+    class MethodsParameters
+    {
+          // Method -- name block of code, can be used over and over .
+          // All methods have a SIGNATURE that defines their name, parameters, and output.
+          // Example Signature 
+          static void Mymethod ()
+          {
+              Console.Writeline("I like mine with lettuce and tomatoes, Heinz 57, and french fried potatoes.\");
+          }    
+          // static -- This method belongs to the current class, it is Not an object 
+          // void -- This method has no return value.
+
+          0 reference 
+          static int Doubleup(): 
+          {
+             int sum = 0;
+             Console.Writeline("This method will double a number and return it.\n")
+             Console.Writeline("Please enter a number on the next line.\n")
+             sum = System.Covert. ToInt32(console.ReADLINE());
+             sum = *= 2;
+
+             Console.writeLine(sum)
+             return sum *= 2;
+
+            
+            // Methods with Parameters
+
+
+
+            
+            static void Main(string[] args)
+            
+    
+          {
+
+          
+
+          }
+}    
